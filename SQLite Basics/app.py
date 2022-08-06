17ecfc740aac13a39ac73c46f924e3547c6e1d7d
SteveRodkiss <SteveRodkiss@users.noreply.github.com>
2022-08-06 16:51:14 +1200

    added from and render template examples

--- SQLite Basics/app.py
+++ SQLite Basics/app.py
@@ -9,13 +9,14 @@ DATABASE = "database.db"
 #routes go here
 @app.route('/')
 def index():
+    #Connect to the datavase file
     db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT * FROM item"
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    db.close()    
-    return render_template('index.html',results=results)
+    cursor = db.cursor()#create the cursor
+    sql = "SELECT * FROM item" #store a query as a string variable
+    cursor.execute(sql) # execute the query
+    results = cursor.fetchall() #fetch all the results of the query (can use fetchone too)
+    db.close()    #closr the database now we are done- no need to commit this time with db.commit()
+    return render_template('index.html',results=results)  #send the data to the template
 
 #this bit of code runs the app that we just made with debug on
 if __name__ == "__main__":
